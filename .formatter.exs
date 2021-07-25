6701647ef2f3dc46d81947c8507de8b3f77a7c56
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-25 10:12:53 +0200

    Added formatter script

---
+++ .formatter.exs
@@ -0,0 +1,15 @@
+[
+  inputs: [
+    ".formatter.exs"
+  ],
+  locals_without_parens: [
+    # Formatter tests
+    assert_format: 2,
+    assert_format: 3,
+    assert_same: 1,
+    assert_same: 2,
+
+    # Errors tests
+    assert_eval_raise: 3
+  ]
+]
