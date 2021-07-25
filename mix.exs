1ac982773f2b34fc1ce4345e10efc81e41f85973
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-25 09:42:10 +0200

    Added mixfile

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Mantra.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :mantra,
+     version: "0.0.1",
+     elixir: "~> 1.1",
+     compilers: Mix.compilers ++ [:mantra],
+     escript: [main_module: Mantra],
+     docs: [readme: true, main: "README.md"],
+     description: """
+     Mantra: A duck-typed, dynamic oop language with a fully redundant actor-model, running on top of the BEAM architecture.
+     """,
+     deps: deps,
+     package: package]
+  end
+
+  defp deps do
+    # [{:erlport, github: "hdima/erlport"}]
+    []
+  end
+
+  defp package do
+    %{
+      licenses: ["BSD-3.0"],
+      maintainers: ["Timo Sarkar"],
+      links: %{ "GitHub" => "https://github.com/timo-cmd2/mantra" }
+    }
+  end
+end
